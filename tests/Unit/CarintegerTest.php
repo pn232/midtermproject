c8d1a90151489e9bfdee4ab076cae547f7956d27
pranav <pranavnanduri@gmail.com>
2018-03-29 18:08:27 -0400

    created a unit test to check if the car year is an integer.

---
+++ tests/Unit/CarintegerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+class CarintegerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $car = Car::inRandomOrder()->first();
+        $year= (int)$car->year;
+        $this->assertInternalType('int', $year);
+    }
+}
